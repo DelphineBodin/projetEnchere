8ce6ddb95b6b8116b619997b98b002db1baa8a69
GllmN <guillaume.neau@outlook.fr>
2021-02-08 14:26:13 +0100

    Modif Guillaume
    Signed-off-by: GllmN <guillaume.neau@outlook.fr>

---
+++ src/fr/eni/projetEnchere/bo/Utilisateur.java
@@ -0,0 +1,19 @@
+package fr.eni.projetEnchere.bo;
+
+public class Utilisateur {
+// A revoir champs Java noUtilisateur, codePostal, motDePasse > SQL no_utilisateur ?
+	private int noUtilisateur ;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private String codePostal;
+	private String ville;
+	private String motDePasse;
+	
+	
+	
+	// Coucou Guillaume et Brice
+}
