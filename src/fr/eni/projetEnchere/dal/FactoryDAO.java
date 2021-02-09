5f122da282617f87dbdb437cfa2ed382a4d2d27e
rroum <rroum@192.168.1.22>
2021-02-09 15:14:07 +0100

    Delphine : Connection avec la base de donnée context.xml - dans lib le jar, connexionprovider, dalException, Factory et TestConnection

---
+++ src/fr/eni/projetEnchere/dal/FactoryDAO.java
@@ -0,0 +1,6 @@
+package fr.eni.projetEnchere.dal;
+
+public abstract class FactoryDAO {
+
+//Notre place pour Fabriquer nos Classes implémentées ex Utilisateur(surrement) DAO qui crééra UtilisateurDAOJdbcImpl() 	
+}
