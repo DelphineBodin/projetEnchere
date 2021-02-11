498044efe12986ddbf1e219b56de292ed46687e8
Hika00 <Hika00@DESKTOP-80E110I.home>
2021-02-11 09:09:00 +0100

    Brice- recuperation champ utilisateur, password en cours

--- WebContent/WEB-INF/jsp/connexion.jsp
+++ WebContent/WEB-INF/jsp/connexion.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,20 +8,28 @@
 </head>
 <body>
 
-<h1> Page de Connexion titre a enlever</h1>
+	<h1>Page de connexion, titre a retirer ensuite</h1>
+
+	<form action="./connexion" method="post">
+		<table>
+			<tr>
+				<td>Identifiant:</td>
+				<td><input name="sidentifiant" placeholder="NineJea"
+					type="text"></td>
+			</tr>
+			<tr>
+				<td>Mot de Passe:</td>
+				<td><input name="smotdepasse" placeholder="******" type="text"></td>
+			</tr>
+		</table>
+
+		<button type="submit" name="ssubmit" value="Connexion">
 
 
-<p>identifiant label</p>
+			<!-- 		<input type="checkbox" name="ssouvenirdemoi">Se souvenir de moi</input> -->
 
-<p>mot de passe label</p>
-
-<p>button connexion</p>
-<p>checkbox se souvenir de moi</p>
-<p>ancre mdp oublié</p>
-
-
-<p>button creer compte</p>
-
+			<!-- 		<input type="submit" name="screeruncompte" value="Créer un compte"> -->
+	</form>
 
 </body>
 </html>
--- src/fr/eni/projetEnchere/servlet/PageConnexionUtilisateur.java
+++ src/fr/eni/projetEnchere/servlet/PageConnexionUtilisateur.java
@@ -8,24 +8,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(name ="PageConnexionUtilisateur",
-value = {"/ConnexionUtilisateur"})
-
+//@WebServlet(name ="PageConnexionUtilisateur",
+//value = {"/ConnexionUtilisateur"})
+@WebServlet("/connexion")
 
 public class PageConnexionUtilisateur extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-  
-    public PageConnexionUtilisateur() {
-        super();
-    }
+
+	public PageConnexionUtilisateur() {
+		super();
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/connexion.jsp").forward(request, response);
 	}
 
+
+	//methode pourra acceder aux parametres des champs remplis par l'utilisateur de la jsp 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		//on s'assure que tomcat decodera les informations reçues avc le coade utf-8
+		request.setCharacterEncoding("UTF-8");
+
+		//recuperation ds parametre identifiant
+		String identifiant="";
+		identifiant = request.getParameter("sidentifiant");
+
+		System.out.println(identifiant);
+
+		//recuperation ds parametre mot de passe
+		String motdepasse="";
+		
+		if(motdepasse.trim().isEmpty() || motdepasse == null) {
+			System.out.println("Veuillez rentrer un mot de passe valide");
+		}else {
+			motdepasse = request.getParameter("smotdepasse");	
+			System.out.println(motdepasse);
+		}
+		
+
+	
+
+
+
 	}
 
 }
