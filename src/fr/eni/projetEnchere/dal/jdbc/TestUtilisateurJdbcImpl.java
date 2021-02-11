a6977b49ba0a8b1a52c8d7270e2341de53494a46
GllmN <guillaume.neau@outlook.fr>
2021-02-11 09:12:06 +0100

    Guillaume - Finalisation UtilisateurDAOJdbcImpl et travail sur TestUtilisateurJdbcImpl

---
+++ src/fr/eni/projetEnchere/dal/jdbc/TestUtilisateurJdbcImpl.java
@@ -0,0 +1,24 @@
+package fr.eni.projetEnchere.dal.jdbc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.projetEnchere.bo.Utilisateur;
+import fr.eni.projetEnchere.dal.UtilisateurDAO;
+
+@WebServlet("/TestUtilisateurJdbcImpl")
+public class TestUtilisateurJdbcImpl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Utilisateur u1 = new Utilisateur(6,"Renelataupe","René","Dupont","renetaupe@laposte.net","0610203040","4 rue du soleil", "44000","Nantes","lataupe","0","0");
+		UtilisateurDAO u = new UtilisateurDAOJdbcImpl();
+	}
+
+
+}
--- src/fr/eni/projetEnchere/dal/jdbc/UtilisateurDAOJdbcImpl.java
+++ src/fr/eni/projetEnchere/dal/jdbc/UtilisateurDAOJdbcImpl.java
@@ -1,5 +1,103 @@
 package fr.eni.projetEnchere.dal.jdbc;
 
-public class UtilisateurDAOJdbcImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import fr.eni.projetEnchere.bo.Utilisateur;
+import fr.eni.projetEnchere.dal.ConnexionProvider;
+import fr.eni.projetEnchere.dal.DALException;
+
+public class UtilisateurDAOJdbcImpl  {
+
+
+	
+
+	public void addUtilisateur(Utilisateur u) throws DALException{
+		Connection cnx = null;
+		PreparedStatement pstmt = null;
+		
+		/*
+		 * if(u==null) { throw new DALException("Pas d'utilisateur en paramêtre"); }
+		 * Connection cnx = ConnexionProvider.seConnecter();
+		 */
+		try {
+//			// desactivation de l'auto commit
+//			cnx.setAutoCommit(false);
+			
+			String requeteSQL = "INSERT into UTILISATEURS (pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+			
+			// Etape 3 : Création de la requête paramétrée et insertion des paramètres dans la requête
+			pstmt = cnx.prepareStatement(requeteSQL, Statement.RETURN_GENERATED_KEYS);
+			
+			// Etape 4 : Remplacer les ? (valoriser les parametres de la requete) 
+			pstmt.setString(1, u.getPseudo());
+			pstmt.setString(2, u.getNom());
+			pstmt.setString(3, u.getPrenom());
+			pstmt.setString(4, u.getEmail());
+			pstmt.setString(5, u.getTelephone());
+			pstmt.setString(6, u.getRue());
+			pstmt.setString(7, u.getCodePostal());
+			pstmt.setString(8, u.getVille());
+			pstmt.setString(9, u.getMotDePasse());
+			pstmt.setInt(10, u.getCredit());
+			pstmt.setBoolean(11,u.getAdministrateur());
+			
+			//Exécution de la requête
+			pstmt.executeUpdate();
+			
+			// récupération de la valeur de identity pour noUtilisateur
+			ResultSet rs = pstmt.getGeneratedKeys();
+			// Chargement de la valeur dans l'objet Utilisateur
+			if(rs.next()) {
+				u.setNoUtilisateur(rs.getInt(1));
+			}
+			
+			// fermeture de resultatset et du prepareStatement
+			rs.close();
+			pstmt.close();
+			
+			// si j'arrive jusqu'à la je peux committer
+			cnx.commit();
+			
+			
+//		}catch (SQLException e) {
+//			try {	
+//				// si je tombe ici, je reviens en arrière
+//				cnx.rollback();
+//			}catch (SQLException e1) {
+//				throw new DALException("Erreur lors de l'ajout d'un utilisateur: " + e1.getMessage());
+//			}// Fin du deuxième catch
+//			throw new DALException(e.getMessage());
+//			//Fin du premier catch
+//			
+//		}finally {
+//			try {
+//				//réatcivation de l'auto commit
+//				cnx.setAutoCommit(true);
+//				cnx.close();
+//			} catch (SQLException e) {
+//				throw new DALException(e.getMessage());
+			
+		} catch (SQLException e) {
+			throw new DALException("Erreur lors de l'ajout d'un utilisateur: " + u, e);
+		}finally {
+			try {
+				if(pstmt!=null) {
+					pstmt.close();
+				}
+				if(cnx!=null) {
+					cnx.close();
+				}
+			}catch (SQLException e) {
+				throw new DALException("Erreur lors de l'ajout d'un utilisateur: " + u);
+			}
+		
+		}
+			
+	}
+		
 }
