f2ef1387d6c6783b72d8688ccb14b285d161e81c
GllmN <guillaume.neau@outlook.fr>
2021-02-12 10:11:49 +0100

    Guilllaume : travail sur BLL

--- src/fr/eni/projetEnchere/bll/AnnuaireUtilisateurManager.java
+++ src/fr/eni/projetEnchere/bll/AnnuaireUtilisateurManager.java
@@ -12,32 +12,92 @@ import fr.eni.projetEnchere.dal.UtilisateurDAO;
 public class AnnuaireUtilisateurManager {
 	
 	private UtilisateurDAO daoUtilisateur;
+	static AnnuaireUtilisateurManager annuaire;
 	
+	// Constructeur privé pour mise en place du pattern Singleton	
 	public AnnuaireUtilisateurManager() {
-		//this.daoUtilisateur = FactoryDAO.getUtilisateurDAO() ;
+		this.daoUtilisateur = FactoryDAO.getUtilisateurDAO() ;
 	}
 	
-//	//Méthode permettant d'acceder à l'ensemble des utilisateurs de l'application
-//	public List<Utilisateur> getCatalogue() throws BLLException{
-//		List<Utilisateur> listeUtilisateur;
-//		try {
-//			listeUtilisateur = this.daoUtilisateur.selectAll();
-//		} catch (DALException e) {
-//			throw new BLLException("Erreur lors de la récupération du catalogue", e);
-//		}
-//		return listeUtilisateur;
-//	}
+	// Méthode pour créer u.... ???????????????????????????????????????
+	public static AnnuaireUtilisateurManager getInstance() {
+		if(annuaire==null) {
+			annuaire= new AnnuaireUtilisateurManager();
+		}
+		return annuaire;
+	}
 	
 	//Ajout d'un utilisateur à notre application
-	public void addUtilisateur(Utilisateur u) throws BLLException {
-		addUtilisateur(u);
+	public boolean validerInscription(Utilisateur u) throws BLLException {
+		StringBuilder messageErreur = new StringBuilder();
+		boolean inscriptionValide = true;
+		// Régle métier :
+		// L'utilisateur ne doit pas être null
+			if(u==null)	{
+				throw new BLLException("utilisateur est null");
+			}
+			// Le pseudo ne peut pas être vide et ne peut pas contenir d'espace
+			if(u.getPseudo()==null || u.getPseudo().trim().isEmpty()) {
+				messageErreur.append("Le pseudo est obligatoire et ne doit pas comporter d'espace\n");
+				inscriptionValide = false;
+			}
+				
+			if(u.getNom()==null || u.getNom().isEmpty()) {
+				messageErreur.append("Le nom est obligatoire\n");
+				inscriptionValide = false;
+			}
+			
+			if(u.getPrenom()==null || u.getPrenom().isEmpty()) {
+				messageErreur.append("Le prenom est obligatoire\n");
+				inscriptionValide = false;
+			}
+			
+			// limiter la taille des caractères à 20
+			if(u.getEmail()==null || u.getEmail().trim().isEmpty()) {
+				messageErreur.append("L'adresse email est obligatoire et ne doit pas comporter d'espace\n");
+				inscriptionValide = false;
+			}
+			
+			if(u.getTelephone()==null || u.getTelephone().trim().isEmpty()) {
+				messageErreur.append("Le numero de telephone est obligatoire et ne doit pas comporter d'espace\n");
+				inscriptionValide = false;
+			}
+			
+			if(u.getRue()==null || u.getRue().isEmpty()) {
+				messageErreur.append("La rue est obligatoire\n");
+				inscriptionValide = false;
+			}
+			
+			// limiter la taille des caractères à 5 et que des chiffres
+			if(u.getCodePostal()==null || u.getCodePostal().trim().isEmpty()) {
+				messageErreur.append("Le code postal est obligatoire et ne doit pas comporter d'espace\n");
+				inscriptionValide = false;
+			}
+			
+			if(u.getVille()==null || u.getVille().trim().isEmpty()) {
+				messageErreur.append("Le nom de la ville est obligatoire et ne doit pas comporter d'espace\n");
+				inscriptionValide = false;
+			}
+			
+			if(u.getMotDePasse()==null || u.getMotDePasse().trim().isEmpty()) {
+				messageErreur.append("Le mot de passe est obligatoire et ne doit pas comporter d'espace\n");
+				inscriptionValide = false;
+			}
+			
+		validerInscription(u);
 		try {
 			this.daoUtilisateur.addUtilisateur(u);
 		} catch (DALException e) {
 			throw new BLLException("Echec ajout utilisateur", e);			
 		}
-	}
+		return inscriptionValide;
+}
 	
+	//Pas de contrainte de renseignement sur : crédit, administrateur et article vendu
+	
+	
+	
+
 //	//Mise à jour des données d'un utilisateur à notre application
 //	public void uptdateUtilsateur(Utilisateur u) throws BLLException {
 //		try {
@@ -56,69 +116,27 @@ public class AnnuaireUtilisateurManager {
 //		} 
 //	}
 	
-	// Lecture d'un utilisateur depuis son identifiant
-	public Utilisateur getUtilisateur(String pseudo) throws BLLException {
-		try {
-			return this.daoUtilisateur.selectByPseudo(pseudo);
-		} catch (DALException e){
-			throw new BLLException("Echec getUtilisateur :" + pseudo, e);
-		}
-	}
-	
-	// Valider des données d'un utilisateur
-	public void validerUtilisateu(Utilisateur u) throws BLLException {
-		StringBuilder messageErreur = new StringBuilder();
-		boolean utilisateurValide = true;
-		if(u==null) {
-			throw new BLLException("Utilisateur null\n");
-		}
-		
-		// Le pseudo ne peut pas être vide et ne peut pas contenir d'espace
-		if(u.getPseudo()==null || u.getPseudo().trim().isEmpty()) {
-			messageErreur.append("Le pseudo est obligatoire\n");
-			utilisateurValide = false;
-		}
-		
-		if(u.getNom()==null || u.getNom().trim().isEmpty()) {
-			messageErreur.append("Le nom est obligatoire\n");
-			utilisateurValide = false;
-		}
-		
-		if(u.getPrenom()==null || u.getPrenom().trim().isEmpty()) {
-			messageErreur.append("Le prenom est obligatoire\n");
-			utilisateurValide = false;
-		}
-		
-		if(u.getEmail()==null || u.getEmail().trim().isEmpty()) {
-			messageErreur.append("L'adresse email est obligatoire\n");
-			utilisateurValide = false;
-		}
-		
-		if(u.getTelephone()==null || u.getTelephone().trim().isEmpty()) {
-			messageErreur.append("Le numero de telephone est obligatoire\n");
-			utilisateurValide = false;
-		}
-		
-		if(u.getRue()==null || u.getRue().trim().isEmpty()) {
-			messageErreur.append("La rue est obligatoire\n");
-			utilisateurValide = false;
-		}
-		
-		if(u.getCodePostal()==null || u.getCodePostal().trim().isEmpty()) {
-			messageErreur.append("Le code postal est obligatoire\n");
-			utilisateurValide = false;
-		}
-		
-		if(u.getVille()==null || u.getVille().trim().isEmpty()) {
-			messageErreur.append("Le nom de la ville est obligatoire\n");
-			utilisateurValide = false;
-		}
-		
-		if(u.getMotDePasse()==null || u.getMotDePasse().trim().isEmpty()) {
-			messageErreur.append("Le mot de passe est obligatoire\n");
-			utilisateurValide = false;
-		}
-		
-		//Pas de contrainte de renseignement sur : crédit, administrateur et article vendu
-	}
+//	// Lecture d'un utilisateur depuis son identifiant
+//	public Utilisateur getUtilisateur(String pseudo) throws BLLException {
+//		try {
+//			return this.daoUtilisateur.selectByPseudo(pseudo);
+//		} catch (DALException e){
+//			throw new BLLException("Echec getUtilisateur :" + pseudo, e);
+//		}
+//	}
+//	
+//	//Méthode permettant d'acceder à l'ensemble des utilisateurs de l'application
+//	public List<Utilisateur> getCatalogue() throws BLLException{
+//		List<Utilisateur> listeUtilisateur;
+//		try {
+//			listeUtilisateur = this.daoUtilisateur.selectAll();
+//		} catch (DALException e) {
+//			throw new BLLException("Erreur lors de la récupération du catalogue", e);
+//		}
+//		return listeUtilisateur;
+//	}
+//		
+//		//Pas de contrainte de renseignement sur : crédit, administrateur et article vendu
+//	}
+
 }
--- src/fr/eni/projetEnchere/dal/FactoryDAO.java
+++ src/fr/eni/projetEnchere/dal/FactoryDAO.java
@@ -1,6 +1,7 @@
 package fr.eni.projetEnchere.dal;
 
 import fr.eni.projetEnchere.dal.jdbc.ArticleDAOImpl;
+import fr.eni.projetEnchere.dal.jdbc.UtilisateurDAOImpl;
 
 public abstract class FactoryDAO {
 	
@@ -8,6 +9,10 @@ public abstract class FactoryDAO {
 
 public static ArticleDAO getArticleDao() {
 	return new ArticleDAOImpl();
+}
+
+public static UtilisateurDAO getUtilisateurDAO() {
+	return new UtilisateurDAOImpl();
 }
 
 }
